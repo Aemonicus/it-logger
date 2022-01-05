2474ad7eedd2c1d73df64ebd80f05cccc56fc09f
Florent VICENTE <vicentegroupe@gmail.com>
2022-01-05 12:10:43 +0100

    Components & UI

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Fragment, useEffect } from 'react';
+import { Searchbar } from "./components/layout/Searchbar"
+import { AddBtn } from "./components/layout/AddBtn"
+import { Logs } from "./components/logs/Logs"
+import { AddLogModal } from "./components/logs/AddLogModal"
+import { EditLogModal } from "./components/logs/EditLogModal"
+import { AddTechModal } from "./components/techs/AddTechModal"
+import { TechListModal } from "./components/techs/TechListModal"
+import './App.css';
+
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const App = () => {
+
+  useEffect(() => {
+    // Initialize Materialize JS
+    M.AutoInit()
+  })
+
+  return (
+    <Fragment>
+      <Searchbar />
+      <div className="container">
+        <AddBtn />
+        <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
+        <Logs />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
